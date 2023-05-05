054195a2cd5259c4efc8baa6a06271e3ea6c9eae
MaxVEngelhard <maxvengelhard@gmail.com>
2023-05-04 21:26:16 -0500

    adding in mouseenter and hieght menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,21 @@
-// components/Layout.js
 import React from 'react';
 
 export function Menu() {
-  return (
-    <>
-      <nav className="flex flex-col h-screen bg-gray-100 w-1/6 border-r border-gray-500">
-        <div className="p-4">Github</div>
-        <div className="p-4">Easy Layers</div>
-        <div className="p-4">React Roulette</div>  
+    const handleHover = (e) => {
+        // TODO: Add your logic to display the image on hover
+      };
+    
+      return (
+        <nav className="flex flex-col h-screen bg-gray-100 w-1/6 border-r border-gray-500">
+          <div className="p-4 border-t border-b h-1/3 hover:bg-gray-200 hover:text-blue-600 cursor-pointer" onMouseEnter={handleHover}>
+            Github
+          </div>
+          <div className="p-4 border-t border-b h-1/3 hover:bg-gray-200 hover:text-blue-600 cursor-pointer" onMouseEnter={handleHover}>
+            Easy Layers
+          </div>
+          <div className="p-4 border-t border-b h-1/3 hover:bg-gray-200 hover:text-blue-600 cursor-pointer" onMouseEnter={handleHover}>
+            React Roulette
+          </div>
         </nav>
-    </>
-  );
+      );
 }
