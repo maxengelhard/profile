3d2153cf9d54fa39d61630cb5a2410e32dfc94c6
MaxVEngelhard <maxvengelhard@gmail.com>
2023-05-05 08:01:09 -0500

    Adding on hover stay when going off

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function Menu() {
-    const handleHover = (e) => {
-        // TODO: Add your logic to display the image on hover
-      };
-    
-      return (
-        <nav className="flex flex-col h-screen bg-gray-100 w-1/6 border-r border-gray-500">
-          <div className="p-4 border-t border-b h-1/3 hover:bg-gray-200 hover:text-blue-600 cursor-pointer" onMouseEnter={handleHover}>
-            About Me
-          </div>
-          <div className="p-4 border-t border-b h-1/3 hover:bg-gray-200 hover:text-blue-600 cursor-pointer" onMouseEnter={handleHover}>
-            Easy Layers
-          </div>
-          <div className="p-4 border-t border-b h-1/3 hover:bg-gray-200 hover:text-blue-600 cursor-pointer" onMouseEnter={handleHover}>
-            React Roulette
-          </div>
-        </nav>
-      );
+  const [activeItem, setActiveItem] = useState(null);
+
+  const handleHover = (e) => {
+    setActiveItem(e.target.innerText);
+    // TODO: Add your logic to display the image on hover
+  };
+
+//   const handleLeave = (e) => {
+//     setActiveItem(null);
+//   };
+
+  const getItemClass = (text) => {
+    if (text === activeItem) {
+      return "bg-gray-200 text-blue-600";
+    } else {
+      return "hover:bg-gray-200 hover:text-blue-600";
+    }
+  };
+
+  return (
+    <nav className="flex flex-col h-screen bg-gray-100 w-1/6 border-r border-gray-500">
+      <div className={`p-4 border-t border-b h-1/3 cursor-pointer ${getItemClass("About Me")}`} onMouseEnter={handleHover}>
+        About Me
+      </div>
+      <div className={`p-4 border-t border-b h-1/3 cursor-pointer ${getItemClass("Easy Layers")}`} onMouseEnter={handleHover}>
+        Easy Layers
+      </div>
+      <div className={`p-4 border-t border-b h-1/3 cursor-pointer ${getItemClass("React Roulette")}`} onMouseEnter={handleHover}>
+        React Roulette
+      </div>
+    </nav>
+  );
 }
