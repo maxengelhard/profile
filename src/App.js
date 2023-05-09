78ccf2687c4734e2ad5ff7a6292de213ecdc5ddf
MaxVEngelhard <maxvengelhard@gmail.com>
2023-05-09 14:21:26 -0500

    Adding routes back to homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route , Navigate } from 'react-router-dom';
 // components
 import { Menu } from "./components/Menu"
 import { ImageDisplay } from "./components/ImageDisplay"
@@ -9,11 +10,18 @@ import aboutmesrc from './media/about-me.png'
 export default function App() {
   const [activeImage, setActiveImage] = useState(aboutmesrc);
   return (
-    <div className='flex'>
-    <Menu onHover={setActiveImage} />
-    <div className="flex-1 overflow-hidden">
-    <ImageDisplay src={activeImage} />
-    </div>
-    </div>
+    <Router>
+    <Routes>
+          <Route path="/" element={
+          <div className='flex'>
+            <Menu onHover={setActiveImage} />
+              <div className="flex-1 overflow-hidden">
+                <ImageDisplay src={activeImage} />
+              </div>
+          </div>} 
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    </Router>
   )
 }
