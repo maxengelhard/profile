5b22ac4ed17fc3af435d6146c00b82db456da7ca
MaxVEngelhard <maxvengelhard@gmail.com>
2023-05-12 17:20:51 -0500

    Cleaning up about me

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,17 +1,28 @@
 export function Description(props) {
-    if (props.src==='about-me') { 
-    return (
+    if (props.src === 'about-me') {
+      return (
         <div>
-            I'm a software / data engineer tha'ts passoniate about techonlogy and how it can solve problems.
-            <button href='https://github.com/maxengelhard'>Github</button>
+          <h1 className="text-2xl font-bold">About Me</h1>
+          <p>
+            I'm a software/data engineer passionate about technology and how it can solve problems.
+          </p>
+          <br />
+          <a
+            href="https://github.com/maxengelhard"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Checkout my Github
+          </a>
         </div>
-    )
-     }
-     else if (props.src==='easy-layers') {
-        return (
-            <div>
-                Easy Layers
-            </div>
-            )
-     }
-}
+      );
+    } else if (props.src === 'easy-layers') {
+      return (
+        <div>
+          <h1 className="text-2xl font-bold">Easy Layers</h1>
+          <p>
+            Description for the Easy Layers feature goes here.
+          </p>
+        </div>
+      );
+    }
+  }
