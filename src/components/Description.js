9ce0df4c0aa9394053ca13a70b4bbf501a5047ea
MaxVEngelhard <maxvengelhard@gmail.com>
2023-05-14 14:35:02 -0500

    Adding new resume and correct link

--- src/components/Description.js
+++ src/components/Description.js
@@ -20,7 +20,7 @@ export function Description(props) {
             Checkout my Github
           </a>
           <a
-            href="https://maxengelhard.com/MaxEngelhardResume.pdf"
+            href="https://www.maxengelhard.com/MaxEngelhardResume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
