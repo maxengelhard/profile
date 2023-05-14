2e0d5056b316840001aac477d9d72823d1b078c5
MaxVEngelhard <maxvengelhard@gmail.com>
2023-05-14 14:30:11 -0500

    Adding resume and link

--- src/components/Description.js
+++ src/components/Description.js
@@ -10,15 +10,24 @@ export function Description(props) {
             In my spare time, I enjoy spending time with my family and friends, working out, and traveling. I'm a strong believer in learning through doing and enjoy working on side projects to help me build my engineering skills.
           </p>
           </div>
-          <div className="w-1/6 flex justify-center items-center pl-1">
+          <div className="w-1/6 pl-1">
           <a
             href="https://github.com/maxengelhard"
-            target="_blank" rel="noopener noreferrer"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
           >
             Checkout my Github
           </a>
-          </div>
+          <a
+            href="https://maxengelhard.com/MaxEngelhardResume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Checkout my resume
+          </a>
+        </div>
         </div>
       );
     } else if (props.src === 'easy-layers') {
