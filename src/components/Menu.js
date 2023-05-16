b6311e16695b32bdf184224e29017aab95158ac0
MaxVEngelhard <maxvengelhard@gmail.com>
2023-05-15 20:16:10 -0500

    Adding black borders

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -37,10 +37,10 @@ export function Menu(props) {
 
   return (
     <nav className="flex flex-col h-screen bg-gray-100 w-1/6 border-r border-gray-500">
-      <div className={`p-4 h-1/3 cursor-pointer ${getItemClass("About Me")}`} onMouseEnter={handleHover}>
+      <div className={`p-4 border-b border-black h-1/3 cursor-pointer ${getItemClass("About Me")}`} onMouseEnter={handleHover}>
         About Me
       </div>
-      <div className={`p-4 h-1/3 cursor-pointer ${getItemClass("Easy Layers")}`} onMouseEnter={handleHover}>
+      <div className={`p-4 border-b border-black h-1/3 cursor-pointer ${getItemClass("Easy Layers")}`} onMouseEnter={handleHover}>
         Easy Layers
       </div>
       <div className={`p-4 h-1/3 cursor-pointer ${getItemClass("React Roulette")}`} onMouseEnter={handleHover}>
